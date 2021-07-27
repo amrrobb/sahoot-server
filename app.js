35cf3511696b7d5637d32301ad9fe4c15d8e3df4
amrrobb <amr.robb@gmail.com>
2021-07-27 23:44:26 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
 require('dotenv').config()
 const express = require("express")
-// const { connect } = require("./config/mongodb")
 const app = express()
 const cors = require('cors')
 
 app.use(cors())
-// const port = 4001
 
 const router = require('./routes')
 
